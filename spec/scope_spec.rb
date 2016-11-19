8059e12f4359d2685b6df77e44900a4bf5ce0a76
Gedean <gedean.dias@gmail.com>
2016-11-19 11:13:30 -0300

    Adding tests

---
+++ spec/scope_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../lib/scope'
+
+describe Scope do 
+	it 'makes a valid scope string' do 
+		expect(Scope.build_scope_string 2016, 10).to eq '201610'
+	end
+
+	it 'makes a valid scope instance' do 
+		expect(Scope.make_scope(2016, 10).value).to eq '201610'
+	end
+
+	it 'returns true to a valid month' do
+		expect(Scope.valid_month? 10).to eq true
+	end
+
+	it 'returns false to a invalid month' do
+		expect(Scope.valid_month? 13).to .eq false
+	end
+
+	it 'returns true to a valid year' do
+		expect(Scope.valid_year? 2016).eq true
+	end
+	
+	it 'returns false to a invalid year' do
+		expect(Scope.valid_year? 3001).eq false
+	end
+
+	it 'returns true to a valid scope' do
+		expect(Scope.valid? 201610).to eq true
+	end
+
+	it 'returns a valid month' do
+		expect(Scope.month '201610').to eq 10
+	end
+
+	it 'returns a valid year' do
+		expect(Scope.year '201610').to eq 2016
+	end
+
+	it 'creates a valid scope' do
+		expect(Scope.new(201610)).to eq true
+	end
+end
